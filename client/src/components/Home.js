dcf0d545a5020aa84513fb8570bf49e3f1ae740a
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-28 11:54:34 -0400

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome to Fantasy Football Game</h1>
+      <p>Please sign up or log in to get started.</p>
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Log In</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
