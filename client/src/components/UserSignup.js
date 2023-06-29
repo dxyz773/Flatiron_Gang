73d84242441c1a207862d9e86a84ba0fa2c52005
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-29 10:30:57 -0400

    added signup and login

--- client/src/components/UserSignup.js
+++ client/src/components/UserSignup.js
@@ -1,5 +1,66 @@
+import React, { useState } from 'react';
+
 function UserSignup() {
-  return <div></div>;
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Function to handle form submission for user signup
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, username, password }),
+      });
+      // Handle the response after user signup
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>User Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default UserSignup;
